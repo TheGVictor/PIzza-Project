a159ad2b02051ffa18c9b8fe0007f15d5d4f37d5
Gabriel Sancinetti <gabrielsancinetti@gmail.com>
2023-04-25 22:32:38 -0300

    Adicionada as funções de marcar o tamanho da pizza e aumentar suas quantidades. Animação na seleção de tamanhos foi implementada no CSS

--- script.js
+++ script.js
@@ -60,4 +60,24 @@ cA('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach((item) =>
     item.addEventListener('click', closeModal)
 })
 
+c('.pizzaInfo--qtmenos').addEventListener('click', () => {
+    if(modalQt > 1){
+        modalQt--;
+        c('.pizzaInfo--qt').innerHTML = modalQt
+    }
+    
+})
+
+c('.pizzaInfo--qtmais').addEventListener('click', () => {
+    modalQt++;
+    c('.pizzaInfo--qt').innerHTML = modalQt
+})
+
+cA('.pizzaInfo--size').forEach((size, sizeIndex) => {
+    size.addEventListener('click', (e) => {
+        c('.pizzaInfo--size.selected').classList.remove('selected')
+        size.classList.add('selected')
+    })
+})
+
 
